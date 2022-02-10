ce94afea94e449cde23a5bd2d0892e1714a851f5
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-02-10 16:50:16 +0530

    feat: displaying respective content onclick

--- counseling-cards/layout/layout.js
+++ counseling-cards/layout/layout.js
@@ -1,13 +1,18 @@
 let eBooks = document.querySelector(".ebook");
 let video = document.querySelector(".video");
-
+let ebookContent = document.querySelector(".ebook-content");
+let videoContent = document.querySelector(".video-content");
 
 video.addEventListener("click",()=>{
     video.style.borderBottom = "5px solid var(--carlet)";
     eBooks.style.borderBottom = "none";
+    ebookContent.style.display = "none";
+    videoContent.style.display = "grid";
 })
 
 eBooks.addEventListener("click",()=>{
     eBooks.style.borderBottom = "5px solid var(--carlet)";
     video.style.borderBottom = "none";
+    ebookContent.style.display = "grid";
+    videoContent.style.display = "none";
 })
