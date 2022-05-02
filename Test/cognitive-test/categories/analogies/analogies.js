eb07b3d6308a1af092b7b44cd2b1fec13bfb96a5
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-05-02 19:42:48 +0530

    feat: questions solved tracker

--- Test/cognitive-test/categories/analogies/analogies.js
+++ Test/cognitive-test/categories/analogies/analogies.js
@@ -3,6 +3,12 @@
 const correctAnswers = ["BEAK","Horse","Guard","Solid","Flow"];
 const formData = document.querySelector(".test-form");
 const btnSubmit = document.querySelector(".btn-submit");
+const qtOneOptions = document.querySelectorAll(".qt-one");
+const qtTwoOptions = document.querySelectorAll(".qt-two");
+const qtThreeOptions = document.querySelectorAll(".qt-three");
+const qtFourOptions = document.querySelectorAll(".qt-four");
+const qtFiveOptions = document.querySelectorAll(".qt-five");
+const questionTracker = document.querySelectorAll(".question-number");
 var score=0;
 
 function calculateScore(){
@@ -22,3 +28,28 @@ function calculateScore(){
     
 }
 btnSubmit.addEventListener('click',calculateScore);
+qtOneOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[0].style.backgroundColor = "#77DD77";
+    })
+})
+qtTwoOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[1].style.backgroundColor = "#77DD77";
+    })
+})
+qtThreeOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[2].style.backgroundColor = "#77DD77";
+    })
+})
+qtFourOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[3].style.backgroundColor = "#77DD77";
+    })
+})
+qtFiveOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[4].style.backgroundColor = "#77DD77";
+    })
+})
--- Test/cognitive-test/categories/number-series/number-series.js
+++ Test/cognitive-test/categories/number-series/number-series.js
@@ -3,6 +3,12 @@
 const correctAnswers = ["15","22","10","1/8","20"];
 const formData = document.querySelector(".test-form");
 const btnSubmit = document.querySelector(".btn-submit");
+const qtOneOptions = document.querySelectorAll(".qt-one");
+const qtTwoOptions = document.querySelectorAll(".qt-two");
+const qtThreeOptions = document.querySelectorAll(".qt-three");
+const qtFourOptions = document.querySelectorAll(".qt-four");
+const qtFiveOptions = document.querySelectorAll(".qt-five");
+const questionTracker = document.querySelectorAll(".question-number");
 var score=0;
 
 function calculateScore(){
@@ -21,3 +27,28 @@ function calculateScore(){
     localStorage.setItem('score',score);
     
 }
+qtOneOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[0].style.backgroundColor = "#77DD77";
+    })
+})
+qtTwoOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[1].style.backgroundColor = "#77DD77";
+    })
+})
+qtThreeOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[2].style.backgroundColor = "#77DD77";
+    })
+})
+qtFourOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[3].style.backgroundColor = "#77DD77";
+    })
+})
+qtFiveOptions.forEach(option => {
+    option.addEventListener("click",()=>{
+        questionTracker[4].style.backgroundColor = "#77DD77";
+    })
+})
