d03f6501f207c57daf434542f5d0684082e24a36
Neha Aadke <62541582+Aadkeneha@users.noreply.github.com>
2022-05-03 14:42:39 +0530

    result created

--- Test/situational-test/result.js
+++ Test/situational-test/result.js
@@ -1,3 +1,3 @@
 const h1 = document.querySelector('.heading');
 
-h1.textContent = "Predicted Personality: " + localStorage.getItem('personality');
+h1.textContent = "Predicted Personality: " + localStorage.getItem('fit');
